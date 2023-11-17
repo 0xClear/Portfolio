088b7182e3df234795bcc21840f897042c73b084
0xClear <bahadoriborz@gmail.com>
2023-11-17 22:03:04 +0100

    g

--- js/rate_rechner.js
+++ js/rate_rechner.js
@@ -24,3 +24,21 @@ document.addEventListener("DOMContentLoaded", function () {
     monthlyPaymentElement.textContent = monthlyPayment.toFixed(2) + " Euro";
   }
 });
+
+jQuery(document).ready(function ($) {
+  $("#Toggle-mobile-nav").click(function () {
+    $("#page").toggleClass("mobile-nav-opened");
+  });
+
+  $("#page").click(function (e) {
+    var target = e.target;
+
+    var ismobilenavbuttonclicked =
+      $(target).hasClass("navbar-toggler") ||
+      $(target).parent().hasClass("navbar-toggler");
+    if (ismobilenavbuttonclicked) {
+      return;
+    }
+    $("#page").removeClass("mobile-nav-opened");
+  });
+});
